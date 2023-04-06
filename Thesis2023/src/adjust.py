2d85ec6e149bfc632f58adb5f4b41946a07c72c1
Braga19 <126405374+Braga19@users.noreply.github.com>
2023-04-06 17:48:42 +0200

    Delete Thesis2023 directory

--- Thesis2023/src/adjust.py
+++
@@ -1,3 +0,0 @@
-def subset(df, name, rating, attribute, value):
-  '''take a pandas df and output a subset of the df where value is gender, age or polarity'''
-  return df.loc[df[attribute] == value, [name, rating]]
